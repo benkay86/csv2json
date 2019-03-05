b0562400739b789278e83d05068345f589702ffc
Daniel Mason <daniel.mason@apolitical.co>
2019-03-05 16:51:58 +0000

    Added: Feature to turn numerically keyed objects into arrays

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,6 +1,36 @@
 use serde_json::map::Entry as JsonEntry;
+use serde_json::Map as JsonMap;
 use serde_json::Value as JsonValue;
-use std::collections::hash_map::Entry;
+
+pub fn group_numeric_arrays(value: JsonValue) -> JsonValue {
+    // If there are no non-numeric keys we can turn this group into an array.
+    if let JsonValue::Object(object) = value {
+        // Recurse over each element in the object
+        let object: JsonMap<String, JsonValue> = object
+            .into_iter()
+            .map(|(key, value)| {
+                let replacement = group_numeric_arrays(value);
+                (key, replacement)
+            })
+            .collect();
+
+        // Test if this object should be an array
+        let remaining_keys: Vec<bool> = object
+            .keys()
+            .filter(|k| k.parse::<u64>().is_err()) // Find anything the doesn't parse to u64
+            .map(|_| true)
+            .collect();
+
+        if remaining_keys.is_empty() {
+            let values: Vec<JsonValue> = object.values().map(|i| i.to_owned()).collect();
+            json!(values)
+        } else {
+            json!(object)
+        }
+    } else {
+        value
+    }
+}
 
 pub fn dimensional_converter(key: String, value: String, ds: Option<&str>) -> (String, JsonValue) {
     if let Some(separator) = ds {
@@ -15,10 +45,10 @@ pub fn dimensional_converter(key: String, value: String, ds: Option<&str>) -> (S
     (key, json!(value))
 }
 
-pub fn prepare_upsert(entry: Entry<String, JsonValue>, data: JsonValue) -> JsonValue {
+pub fn prepare_upsert(entry: JsonEntry, data: JsonValue) -> JsonValue {
     match entry {
-        Entry::Vacant(_) => data,
-        Entry::Occupied(e) => {
+        JsonEntry::Vacant(_) => data,
+        JsonEntry::Occupied(e) => {
             let old_value = e.remove();
             merge_values(old_value, data)
         }
